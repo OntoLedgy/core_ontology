ce35b73e7297b9967259ff0cdb55a66e67557f6f
Mesbah Khan <khanm@ontoledgy.onmicrosoft.com>
2021-12-28 13:07:30 +0100

    minor dependency fixes and code correction

--- interfaces/i_objects.go
+++ interfaces/i_objects.go
@@ -1,8 +1,10 @@
 package interfaces
 
-import "database_manager/utils"
+import (
+	"github.com/OntoLedgy/storage_interop_services/databases/utils"
+)
 
 type IObjects interface {
-	Set_object_uuid() *utils.UUID
-	Get_object_uuid() *utils.UUID
+	Set_object_uuid() *utils.UUIDs
+	Get_object_uuid() *utils.UUIDs
 }
